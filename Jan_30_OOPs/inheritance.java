56836e0e0886a911606f345c19240348def59e6a
jky-20 <jaiy.it.16@nsit.net.in>
2025-01-30 21:19:16 +0530

    OOPS

---
+++ Jan_30_OOPs/inheritance.java
@@ -0,0 +1,33 @@
+package Jan_30_OOPs;
+
+class employe{
+    int salary;
+    String name;
+    employe(int salary, String name){
+        this.name = name;
+        this.salary = salary;
+    }
+    void print(){
+        System.out.println("Parent class");
+    }
+}
+class engineer extends employe{
+    int bonus;
+    engineer(int bonus){
+        // System.out.println(this.salary);    // salary is not initialized 
+        super(1000, "Jai");
+        this.bonus = bonus;
+    }
+    void print(){
+        super.print();  // access hidden methods in parent class
+        System.out.println("Child class");
+    }
+}
+
+public class inheritance {
+    public static void main(String[] args) {
+        
+        engineer e1 = new engineer(1000);
+        e1.print();
+    }
+}
