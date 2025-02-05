4efc80e2d76f26be25c9dbde1b760a1b6b64141a
jky-20 <jaiy.it.16@nsit.net.in>
2025-02-05 13:01:24 +0530

    Classroom Code

---
+++ Lec_27_Feb_5_OOPs/final_keyword.java
@@ -0,0 +1,13 @@
+package Lec_27_Feb_5_OOPs;
+
+public class final_keyword {
+    public static void main(String[] args) {
+        
+        final int a = 10;
+        // a = 20;     // final variable can't be modfied
+
+        final int b;
+        b = 20;     // intialization
+        // b = 30;
+    }
+}
---
+++ Lec_27_Feb_5_OOPs/overriding.java
@@ -0,0 +1,73 @@
+package Lec_27_Feb_5_OOPs;
+
+class motor_vehicle{
+    motor_vehicle(){
+        System.out.println("Motor Vehicle");
+    }
+    void start(){
+        System.out.println("switch on motor engine");
+    }
+    motor_vehicle get_Vehicle(){
+        return new motor_vehicle();
+    }
+
+    static void print(){
+        System.out.println("static method in parent class");
+    }
+    final void final_func(){
+        System.out.println("Final function");
+    }
+
+    private void func(){
+        System.out.println("private function");
+    }
+}
+
+class car extends motor_vehicle{
+
+    // motor_vehicle(){        // constructors can't be override
+    //     System.out.println("Car Vehicle");
+    // }
+    
+    void func(){   // not a overridden method
+        System.out.println("function");
+    }
+    void start(){
+        System.out.println("Starts by key");
+    }
+    
+    // @Override    // specifies function must override method
+    // void strt(){
+
+    // }
+    car get_Vehicle(){
+        return new car();
+    }
+
+    // void print(){   // can't be overridden
+    //     System.out.println("static method in parent class");
+    // }
+
+    static void print(){    // method hiding
+        System.out.println("Not a overridden function");
+    }
+
+    // void final_func(){   // can't be overridden
+    //     System.out.println("Final function");
+    // }
+}
+
+class bike extends motor_vehicle{
+
+    void start(){
+        System.out.println("Starts by kick");
+    }
+}
+
+public class overriding {
+    public static void main(String[] args) {
+        
+        car c1 = new car();
+        c1.start();     // calls overridden method
+    }
+}
